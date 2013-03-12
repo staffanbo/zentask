d6e338b45cf027edbccbc85e212fa7df5b5b0cad
bornehager <bornehager@10.0.1.13>
2013-03-12 19:43:49 +0100

    homework

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import models.*;
+import play.data.*;
+import static play.data.Form.*;
 
 import views.html.*;
 
@@ -13,6 +15,38 @@ public class Application extends Controller {
                 Project.find.all(),
                 Task.find.all()
         ));
+    }
+    
+    public static Result login() {
+        return ok(
+            login.render(form(Login.class))
+        );
+    }
+    
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
+    
+    public static class Login {
+
+        public String email;
+        public String password;
+
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+              return "Invalid user or password";
+            }
+            return null;
+        }
     }
 }
 
