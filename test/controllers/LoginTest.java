878aad9746216a99e4999adede3f4f8a53546494
bornehager <bornehager@10.0.1.13>
2013-03-12 19:42:32 +0100

    homework

---
+++ test/controllers/LoginTest.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+import play.mvc.*;
+import play.libs.*;
+import play.test.*;
+import static play.test.Helpers.*;
+import com.avaje.ebean.Ebean;
+import com.google.common.collect.ImmutableMap;
+
+public class LoginTest extends WithApplication {
+    @Before
+    public void setUp() {
+        start(fakeApplication(inMemoryDatabase(), fakeGlobal()));
+        Ebean.save((List) Yaml.load("initial-data.yml"));
+    }
+    
+    @Test
+    public void authenticateSuccess() {
+        Result result = callAction(
+        controllers.routes.ref.Application.authenticate(),
+        fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+            "email", "bob@example.com",
+            "password", "secret"))
+        );
+        assertEquals(302, status(result));
+        assertEquals("bob@example.com", session(result).get("email"));
+    }
+
+}
